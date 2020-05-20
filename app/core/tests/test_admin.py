d3960f0d5afc2fea9137066b7e5ee4b652c2d4c6
Joseph James <josephjames@Josephs-MacBook-Air.local>
2020-05-21 00:43:01 +0530

    Admin updated

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_super_user(
+            email="cred@gmail.com",
+            password="Open@123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@123.com",
+            password="Open@123",
+            name="Test user full name"
+        )
+
+    def test_user_listed(self):
+        """Test that the users are listed in the page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """ This test is to check if the user page edit works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_user(self):
+        """This test is to add user"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
